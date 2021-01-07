3288b7d670dd95a044f22fce3ea7ce349e664f5b
Shiho Kanno <shihopoyopoyochan@gmail.com>
2021-01-07 16:42:22 +0100

    Use useState to read input values

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
 
-  function handleOnSubmit(e) {
-    e.preventDefault();
-    console.log("From submitted");
-    console.log(emailInputRef.current.value);
+  // function handleEmailInput(e) {
+  //   console.log(e.target.value);
+  // }
+  // function handlePasswordInput(e) {
+  //   console.log(e.target.value);
+  // }
+  //一つにまとめると以下のようになる。
+
+  function handleOnChange(e) {
+    // console.log(e.target.value);
+    //nameとpasswordを区別できるように変える。
+    console.log(e.target.name, e.target.value);
+    if (e.target.name === "email") {
+      setEmailInput(e.target.value);
+    }
+    if (e.target.name === "password") {
+      setPasswordInput(e.target.value);
+    }
   }
+
   return (
     <div>
-      <form onSubmit={handleOnSubmit}>
+      <form>
         <label>Email</label>
-        <input ref={emailInputRef} />
+        {/* <input onChange={handleEmailInput} /> */}
+        <input name="email" onChange={handleOnChange} />
         <label>Password</label>
-        <input ref={passwordInputRef} />
+        {/* <input onChange={handlePasswordInput} /> */}
+        <input name="password" onChange={handleOnChange} />
         <button type="submit">Log In</button>
       </form>
     </div>
